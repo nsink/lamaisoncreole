63b2be1852c4fb66982298352feea27aabef3ac6
remi <remi.vion@navendis.com>
2013-09-27 22:11:43 +0200

    emails from info@lamaisoncreole and direction@lmc in cc

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 class UserMailer < ActionMailer::Base
-  default from: "support@lamaisoncreole.com"
+  default from: "info@lamaisoncreole.com"
 
   def contact_email(msg)
-    mail(to: ['info@lamaisoncreole.com', msg.email], bcc:['neoryv@laposte.net', 'mvion@lamaisoncreole.com'], subject: 'Contact Hotel La-Maison-Creole', reply_to: msg.email) do |format|
+    mail(to: ['info@lamaisoncreole.com', msg.email], bcc:['neoryv@laposte.net', 'mvion@lamaisoncreole.com', 'direction@lamaisoncreole.com'], subject: 'Contact Hotel La-Maison-Creole', reply_to: msg.email) do |format|
       format.text do
       	render text: "[#{msg.nom}, #{msg.email}, #{msg.telephone}, #{msg.created_at}]: #{msg.message}"
       end
