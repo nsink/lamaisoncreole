a2ad464b64bfe9264f1980a8da44fbbf613464d4
remi <remi.vion@navendis.com>
2013-11-08 13:39:24 +0100

    temp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,28 @@ module ApplicationHelper
     'it' => 'Italiano'}
 	end
 
+  # File actionpack/lib/action_view/helpers/asset_tag_helper.rb, line 356
+  def light_image_tag(source, options = {})
+    options.symbolize_keys!
+    src = options[:src] = path_to_image(source)
+    unless src =~ /^(?:cid|data):/ || src.blank?
+      options[:alt] = options.fetch(:alt){ image_alt(src) }
+    end
+    if size = options.delete(:size)
+      options[:width], options[:height] = size.split("x") if size =~ %{^\d+x\d+$}
+    end
+    if mouseover = options.delete(:mouseover)
+      options[:onmouseover] = "this.src='#{path_to_image(mouseover)}'"
+      options[:onmouseout]  = "this.src='#{src}'"
+    end
+
+    content_tag(:a, tag("img", options), :href => src, :class => "image-link")
+    # content_tag :a, :class => "image-link" do
+      # content_tag :img, options
+    # end
+    # tag("a", href: src) do
+      # tag("img", options)
+    # end
+  end
+
 end
